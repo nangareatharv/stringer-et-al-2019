2cbb1432465440b341dc2c6a4c97b94130c8238a
nangareatharv <155799532+nangareatharv@users.noreply.github.com>
2024-04-13 12:06:37 +0530

    bool syntax

--- decoders.py
+++ decoders.py
@@ -696,7 +696,7 @@ def chron_discrimination(fs, all_depths, npc=0):
 
         # use 75% vs 25%
         itrain = np.zeros((nstim,), bool)
-        itest = np.zeros((nstim,), np.bool)
+        itest = np.zeros((nstim,), bool)
         itrain[:int(nstim*.75)] = True
         itest[int(nstim*.75):] = True
         D1, dy1, A = derivative_decoder(istim, sresp, itrain, itest, lam = 1)
